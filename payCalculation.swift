5761f874cc53c05e5f8ca4a28486d44effba6553
Cod3Connor <100303866+Cod3Connor@users.noreply.github.com>
2024-03-20 09:55:30 -0600

    h\

---
+++ payCalculation.swift
@@ -0,0 +1,13 @@
+//
+//  payCalculation.swift
+//  overtimeIAmChallenged
+//
+//  Created by Ayo what the dog doing Codeconnor on 3/20/24.
+//
+
+import Foundation
+
+protocol PayCalculation{
+    func calculateWage(withBasePay basePay:Double, andHours hours:Double)-> Double
+   
+}
