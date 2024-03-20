466fa04024ef07961b1b1a685d47d8113daf220a
Cod3Connor <100303866+Cod3Connor@users.noreply.github.com>
2024-03-20 08:51:25 -0600

    Initial Commit

---
+++ overtimeIAmChallenged/overtimeIAmChallengedApp.swift
@@ -0,0 +1,17 @@
+//
+//  overtimeIAmChallengedApp.swift
+//  overtimeIAmChallenged
+//
+//  Created by Ayo what the dog doing Codeconnor on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct overtimeIAmChallengedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
